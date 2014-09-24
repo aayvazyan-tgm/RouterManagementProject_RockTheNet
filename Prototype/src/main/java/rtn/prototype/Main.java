34ff4c944208d40e8aa44655478c9a6dd2660736
Jakob Klepp <jklepp@student.tgm.ac.at>
2014-09-24 22:58:17 +0200

    NETSCREEN-POLICY-MIB

--- Prototype/src/main/java/rtn/prototype/Main.java
+++ Prototype/src/main/java/rtn/prototype/Main.java
@@ -1,9 +1,6 @@
 package rtn.prototype;
 
 import java.io.IOException;
-import java.io.OutputStreamWriter;
-
-import org.apache.log4j.*;
 
 /**
  * Main class of prototype
@@ -16,40 +13,23 @@ public class Main {
 
     public static void main(String[] args) {
         runShivasoftTutorial();
+        runQueryNetscreenPolicyMib();
         runQueryOIDs();
     }
 
     private static void runShivasoftTutorial() {
-
-        Logger logger = Logger.getLogger("Workflow");
-        /* may be removed and replaced by a FileAppender in the future */
-        {
-            BasicConfigurator.configure();
-            ConsoleAppender ca = new ConsoleAppender();
-            ca.setWriter(new OutputStreamWriter(System.out));
-            ca.setLayout(new PatternLayout("%-5p [%t]: %m%n"));
-            logger.addAppender(ca);
-        }
-        /*
-        FileAppender ap;
-        try {
-            ap = new FileAppender(new SimpleLayout(), m.getLogVerzeichnis()+File.separator+"log.txt", false); //erstellt einene neuen appender fuer log4J welcher in ein File loggt
-            BasicConfigurator.configure(ap); //der appender wird dem logger zugewiesen
-        } catch (IOException e1) {
-            e1.printStackTrace();
-        }
-        */
-
-        logger.log(Level.INFO, "Starting up");
         try {
             SNMPManager.run(URL);
         } catch (IOException e) {
-            logger.log(Level.INFO, "A Unhandled exception occurred while executing the SNMPManager");
             e.printStackTrace();
         }
     }
 
     private static void runQueryOIDs() {
         QueryOIDs.run(URL);
+    }
+
+    private static void runQueryNetscreenPolicyMib() {
+        NetscreenPolicyMib.run(URL);
     }
 }
