edf48d4dbcf4b8b4262fe0c5ac8b50f421ed1e30
Jakob Klepp <jklepp@student.tgm.ac.at>
2014-09-24 19:12:46 +0200

    set up subproject :Prototype

---
+++ Prototype/src/main/java/rtn/prototype/Main.java
@@ -0,0 +1,10 @@
+package rtn.prototype;
+
+/**
+ * Main class of prototype
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Run main!");
+    }
+}
