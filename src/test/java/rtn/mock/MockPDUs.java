1a369401405b45dadadb9fbf6d48a5c86141ece9
Jakob Klepp <jklepp@student.tgm.ac.at>
2014-09-26 16:52:26 +0200

    MockResponseEvents

---
+++ src/test/java/rtn/mock/MockPDUs.java
@@ -0,0 +1,15 @@
+package rtn.mock;
+
+import org.snmp4j.PDU;
+
+import static org.mockito.Mockito.mock;
+
+/**
+ * Created by jakob on 9/26/14.
+ */
+public class MockPDUs {
+    public static PDU getPDU() {
+        PDU pdu = mock(PDU.class);
+
+    }
+}
