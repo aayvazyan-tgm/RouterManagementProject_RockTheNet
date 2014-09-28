76bc2887d2b0a3310ac19e76019a01a74ee2af66
aayvazyan-tgm <ariay@live.at>
2014-09-28 21:50:56 +0200

    Implemented getters, setters and constructor

--- src/main/java/rtn/networking/Service.java
+++ src/main/java/rtn/networking/Service.java
@@ -4,4 +4,15 @@ public class Service {
 
 	private String name;
 
+    public Service(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
