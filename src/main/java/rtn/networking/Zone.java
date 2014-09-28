507641016a057ff72b8dacebbdadb3caf4fc15fa
aayvazyan-tgm <ariay@live.at>
2014-09-28 21:58:05 +0200

    Implemented getters, setters and constructor

--- src/main/java/rtn/networking/Zone.java
+++ src/main/java/rtn/networking/Zone.java
@@ -4,4 +4,14 @@ public class Zone {
 
 	private String name;
 
+    Zone(String name){
+        this.name=name;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
