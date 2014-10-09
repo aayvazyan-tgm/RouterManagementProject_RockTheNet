fae2f175567467747d246fc4e1f7fae9dbaee7ac
Andreas Willinger <awi95@gmx.at>
2014-10-09 09:58:59 +0200

    Added AddPolicyWindow, fixed build.gradle, fixed MainApp (again)

--- src/main/java/rtn/MainApp.java
+++ src/main/java/rtn/MainApp.java
@@ -25,7 +25,7 @@ public class MainApp extends Application {
 		try {
             // Load root layout from fxml file.
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("gui/view/ConnectionWindow.fxml"));
+            loader.setLocation(getClass().getClassLoader().getResource("rtn/gui/view/ConnectionWindow.fxml"));
             rootLayout = (Pane) loader.load();
 
             // Show the scene containing the root layout.
