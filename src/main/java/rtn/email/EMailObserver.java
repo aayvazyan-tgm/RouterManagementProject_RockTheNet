47305fe1e60fdee81cacf43c7c1f845c975f4227
Ari Ayvazyan <ariay@live.at>
2014-10-10 10:03:29 +0200

    added missing javadoc

--- src/main/java/rtn/email/EMailObserver.java
+++ src/main/java/rtn/email/EMailObserver.java
@@ -1,14 +1,29 @@
 package rtn.email;
 
 
-import java.util.*;
+import org.apache.commons.mail.EmailException;
+import org.apache.log4j.*;
+import rtn.observer.*;
 
 public class EMailObserver implements Observer {
 
 	private EMailWorker eMailWorker;
 
+    /**
+     * notifies the observable
+     * @param observable the notifier
+     */
     @Override
-    public void update(Observable o, Object arg) {
-
+    public void notify(Observable observable) {
+        try {
+            eMailWorker.send(EMailFactory.getMail());
+        } catch (EmailException e) {
+            Logger rtnlog=Logger.getLogger("rtn");
+            rtnlog.log(Level.ERROR,"The E-Mail could not be sent: "+e.getMessage());
+        } catch (CloneNotSupportedException e) {
+            Logger rtnlog=Logger.getLogger("rtn");
+            rtnlog.log(Level.ERROR,"(Internal Error) The E-Mail could not be sent: "+e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
