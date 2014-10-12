a75052bb339fd355f47e80b864cbe7fb08e7caf6
Jakob Klepp <jklepp@student.tgm.ac.at>
2014-10-12 14:03:14 +0200

    copy-pasted examplecode for snmp trap
    src:
    jitendrazaa.com/blog/java/snmp/generating-trap-in-snmp-using-snmp4j

--- TrapPrototype/src/main/java/rtn/prototype/snmp/trap/Main.java
+++ TrapPrototype/src/main/java/rtn/prototype/snmp/trap/Main.java
@@ -1,10 +1,19 @@
 package rtn.prototype.snmp.trap;
 
+import org.snmp4j.smi.UdpAddress;
+
+import java.io.IOException;
+
 /**
  * Created by jakob on 10/9/14.
  */
 public class Main {
     public static void main(String[] args) {
-
+        TrapReceiver snmp4jTrapReceiver = new TrapReceiver();
+        try {
+            snmp4jTrapReceiver.listen(new UdpAddress("localhost/162"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ TrapPrototype/src/main/java/rtn/prototype/snmp/trap/TrapReceiver.java
@@ -0,0 +1,75 @@
+package rtn.prototype.snmp.trap;
+
+import org.snmp4j.*;
+import org.snmp4j.mp.MPv1;
+import org.snmp4j.mp.MPv2c;
+import org.snmp4j.security.Priv3DES;
+import org.snmp4j.security.SecurityProtocols;
+import org.snmp4j.smi.OctetString;
+import org.snmp4j.smi.TcpAddress;
+import org.snmp4j.smi.TransportIpAddress;
+import org.snmp4j.smi.UdpAddress;
+import org.snmp4j.transport.AbstractTransportMapping;
+import org.snmp4j.transport.DefaultTcpTransportMapping;
+import org.snmp4j.transport.DefaultUdpTransportMapping;
+import org.snmp4j.util.MultiThreadedMessageDispatcher;
+import org.snmp4j.util.ThreadPool;
+
+import java.io.IOException;
+
+public class TrapReceiver implements CommandResponder {
+
+    /**
+     * Trap Listner
+     */
+    public synchronized void listen(TransportIpAddress address)
+            throws IOException {
+        AbstractTransportMapping transport;
+        if (address instanceof TcpAddress) {
+            transport = new DefaultTcpTransportMapping((TcpAddress) address);
+        } else {
+            transport = new DefaultUdpTransportMapping((UdpAddress) address);
+        }
+
+        ThreadPool threadPool = ThreadPool.create("DispatcherPool", 10);
+        MessageDispatcher mDispathcher = new MultiThreadedMessageDispatcher(
+                threadPool, new MessageDispatcherImpl());
+
+        // add message processing models
+        mDispathcher.addMessageProcessingModel(new MPv1());
+        mDispathcher.addMessageProcessingModel(new MPv2c());
+
+        // add all security protocols
+        SecurityProtocols.getInstance().addDefaultProtocols();
+        SecurityProtocols.getInstance().addPrivacyProtocol(new Priv3DES());
+
+        // Create Target
+        CommunityTarget target = new CommunityTarget();
+        target.setCommunity(new OctetString("public"));
+
+        Snmp snmp = new Snmp(mDispathcher, transport);
+        snmp.addCommandResponder(this);
+
+        transport.listen();
+        System.out.println("Listening on " + address);
+
+        try {
+            this.wait();
+        } catch (InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    /**
+     * This method will be called whenever a pdu is received on the given port
+     * specified in the listen() method
+     */
+    public synchronized void processPdu(CommandResponderEvent cmdRespEvent) {
+        System.out.println("Received PDU...");
+        PDU pdu = cmdRespEvent.getPDU();
+        if (pdu != null) {
+            System.out.println("Trap Type = " + pdu.getType());
+            System.out.println("Variables = " + pdu.getVariableBindings());
+        }
+    }
+}
