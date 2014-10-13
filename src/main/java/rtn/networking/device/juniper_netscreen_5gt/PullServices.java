bc8eaefc47faaa232c8dec79a7748fdb19bfac8f
ddjuric-tgm <ddjuric@student.tgm.ac.at>
2014-10-13 10:14:29 +0200

    Implementing Pullservices

--- src/main/java/rtn/networking/device/juniper_netscreen_5gt/PullServices.java
+++ src/main/java/rtn/networking/device/juniper_netscreen_5gt/PullServices.java
@@ -1,8 +1,15 @@
 package rtn.networking.device.juniper_netscreen_5gt;
 
+import rtn.networking.Service;
 import rtn.networking.device.commands.IPullServices;
 
 public class PullServices implements IPullServices {
+	
+	private Service service;
+	
+	public PullServices(Service service) {
+		this.service=service;
+	}
 	/**
 	 * @see rtn.networking.device.commands.IResult#getResult()
 	 */
